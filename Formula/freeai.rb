a6eac202a5f12727856dbd5dea81c50718c3951b
astikspandey <147242239+astikspandey@users.noreply.github.com>
2026-01-15 09:41:50 +0530

    Update URL and SHA256 for Freeai formula

--- Formula/freeai.rb
+++ Formula/freeai.rb
@@ -1,8 +1,8 @@
 class Freeai < Formula
   desc "CLI for generating and executing AI-generated shell scripts"
   homepage "https://github.com/astikspandey/freeai"
-  url "https://raw.githubusercontent.com/astikspandey/freeai/v1.0.1/freeai"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://raw.githubusercontent.com/astikspandey/freeai/v1.0.1.1/freeai"
+  sha256 "f2bcc2a36bde572afc966e6c78cff788c4e68717dbd1a51e2d261da98b8b39ea"
   version "1.0.1"
   license "MIT"
 
